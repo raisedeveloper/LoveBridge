8e7130e2ec17bd66448e6839723a3d3fc94144af
develop_yunju <ilove4903@naver.com>
2024-05-15 15:18:09 +0900

    love_bridge

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,7 +3,7 @@ import HeaderAside from "../components/HeaderAside";
 export default function album({x}) {
   const handleFileChange = (event) => {
     const file = event.target.files[0];  
-    console.log('Selected File:', file);
+    console.log('Selected File:', file);  
     // 여기서 선택한 파일을 저장하고 앨범에 표시하는 로직을 추가할 수 있습니다.
   };
 
